9a7d787543118482edfae1bab31731e400d1c427
dev11 <dev11@finoservices.com>
2024-01-25 18:11:01 -0500

    Refactor AddToCard and Header components, and useProductsCardStore

--- src/commons/store/useProductsCardStore.ts
+++ src/commons/store/useProductsCardStore.ts
@@ -1,19 +1,13 @@
 import { create } from 'zustand'
+import type { Product } from '../types/productsApi'
 
+interface ProductsArray extends Array<Product> { }
 interface Products {
-  name: string
-  quantity: number
-  price: number
-  image: string
-  addProduct: (name: string, quantity: number, price: number, image: string) => void
+  products: ProductsArray
+  addProduct: (product: Product) => void
 }
 
-interface ProductsArray extends Array<Product> {}
-
 const useProductsCardStore = create<Products>()((set) => ({
-  name: '',
-  quantity: 0,
-  price: 0,
-  image: '',
-  addProduct: (name, quantity, price, image) => set((state): Products => ([...state, { name, quantity, price, image }]))
+  products: [],
+  addProduct: (product: Product) => set((state) => ({ products: [...state.products, product] }))
 }))
